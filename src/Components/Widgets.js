892e16d4a1d871410bcaa5306699c46df255aa28
Stanimir <stanimirkosev00@gmail.com>
2022-06-02 21:07:19 +0300

    widget component

--- src/Components/Widgets.js
+++ src/Components/Widgets.js
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Widgets() {
-  return <div className="widgets">Widgets</div>;
+  return (
+    <div className="widgets">
+      <div className="widgets-input">
+        <SearchIcon className="widgets-search-icon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets-widget-container">
+        <h2>Trends for you</h2>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="naval"
+          options={{ height: 400 }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Widgets;
