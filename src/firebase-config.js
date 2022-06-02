da4cff287aad25bcb3a3c9c147f7ed702004f3e1
Stanimir <stanimirkosev00@gmail.com>
2022-06-02 12:10:41 +0300

    set up

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXTulGONMy8lfjWvgSbbSTp_Xim7Hc0C8",
+  authDomain: "twitter-clone-e522c.firebaseapp.com",
+  projectId: "twitter-clone-e522c",
+  storageBucket: "twitter-clone-e522c.appspot.com",
+  messagingSenderId: "442898171006",
+  appId: "1:442898171006:web:2ed440703e1c241be1442f",
+};
+
+export const app = initializeApp(firebaseConfig);
